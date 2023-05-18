7420a636c669144b0952341ef32ef36b63484400
Kotaro Miura <miura@irohasoft.jp>
2023-05-18 13:21:19 +0900

    カスタムディレクトリを追加

---
+++ Custom/Config/config.php
@@ -0,0 +1,13 @@
+<?php
+// Custom Config
+$config['dummy']		= [];
+
+// 利用者ログイン画面に管理者ログイン用リンクを表示する場合は以下のコメントを解除してください。
+//$config['show_admin_link'] = true;
+
+// お知らせのリンクを同じ画面で開く場合は以下のコメントを解除してください。
+//$config['open_link_same_window'] = true;
+
+// 独自のテーマカラーを追加する場合には、以下のコメントを解除し、カラーコードを変更してください。
+//$config['theme_colors'][] = ['#000000' => 'custom'];
+
