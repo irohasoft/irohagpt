c9925a7ef0ba11a2d5b2ac641d79c19a25064d4c
Kotaro Miura <miura@irohasoft.jp>
2023-05-25 14:56:43 +0900

    APIのパラメータの設定を追加

--- Config/openai.php
+++ Config/openai.php
@@ -4,3 +4,13 @@
 $config['api_key']	= '(your API Key)';
 $config['model']	= 'gpt-3.5-turbo';
 //$config['model']	= 'gpt-4';
+
+
+// API request parameters
+// https://platform.openai.com/docs/api-reference/chat
+$config['temperature']			= 1.0;
+$config['max_tokens']			= 1024;
+$config['top_p']				= 1.0;
+$config['frequency_penalty']	= 0;
+$config['presence_penalty']		= 0;
+
