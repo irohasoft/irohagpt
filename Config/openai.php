8c3bd1cd5170ce07a4752dff87178da6891d385f
Kotaro Miura <miura@irohasoft.jp>
2024-09-06 17:31:30 +0900

    デフォルトのモデルをgpt-4oに変更

--- Config/openai.php
+++ Config/openai.php
@@ -2,8 +2,9 @@
 
 // Replace your API key
 $config['api_key']	= '(your API Key)';
-$config['model']	= 'gpt-3.5-turbo';
+//$config['model']	= 'gpt-3.5-turbo';
 //$config['model']	= 'gpt-4';
+$config['model']	= 'gpt-4o';
 
 
 // API request parameters
